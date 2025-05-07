84a2f8a5cf2ac362f349fef72252f835b9b08a6c
nisa cibik <nisacibikk@gmail.com>
2025-05-07 16:26:57 +0300

    frontend işlemleri tamamlandı

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ AOS.init({
     duration: 1000,
     once: true,
   });
+  document.addEventListener('DOMContentLoaded', function () {
+    const scrollText = document.querySelector('.scroll-text');
+  
+    function checkScroll() {
+      const rect = scrollText.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+      if (rect.top <= windowHeight * 0.9) {
+        scrollText.classList.add('active');
+        window.removeEventListener('scroll', checkScroll);
+      }
+    }
+  
+    window.addEventListener('scroll', checkScroll);
+    checkScroll();
+  });
+  
